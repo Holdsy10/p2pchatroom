e1896e3721aec36a8377dc5e84be368d70dd29c7
Curtis Holdsworth <n8588244@qut.edu.au>
2019-06-03 23:01:18 +1000

    Working Chat App

--- app/src/main/java/com/example/p2pchatroom/MainActivity.java
+++ app/src/main/java/com/example/p2pchatroom/MainActivity.java
@@ -10,6 +10,7 @@ import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
 import android.net.wifi.WifiManager;
+import android.net.wifi.WpsInfo;
 import android.net.wifi.p2p.WifiP2pConfig;
 import android.net.wifi.p2p.WifiP2pDevice;
 import android.net.wifi.p2p.WifiP2pDeviceList;
@@ -47,7 +48,6 @@ public class MainActivity extends AppCompatActivity {
     TextView read_msg_box, ConnectionStatus, connection1, connection2;
     EditText writeMsg;
 
-
     WifiManager wifiManager;
     WifiP2pManager mManager;
     WifiP2pManager.Channel mChannel;
@@ -60,10 +60,13 @@ public class MainActivity extends AppCompatActivity {
     WifiP2pDevice[] deviceArray;
 
     static final int MESSAGE_READ = 1;
+    final int PORT_NUMBER = 8888;
 
     ServerClass serverClass;
     ClientClass clientClass;
     SendReceive sendReceive;
+
+    boolean isConnected = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,9 +74,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initialWork();
-        exqListener();
-        //wifiOn();
+        //exqListener();
+        autoListener();
     }
+
 
     Handler handler = new Handler(new Handler.Callback() {
         @Override
@@ -92,11 +96,7 @@ public class MainActivity extends AppCompatActivity {
     });
 
 
-    private void wifiOn() {
-        //Turns WiFI on when the app starts if it is off
-        if (!wifiManager.isWifiEnabled()) {
-            wifiManager.setWifiEnabled(true);
-        }
+    private void discoverPeers() {
         //Starts Looking for new peers Once WiFI has been turned on.
         mManager.discoverPeers(mChannel, new WifiP2pManager.ActionListener() {
             @Override
@@ -110,9 +110,18 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
 
+    private void turnWifiOn() {
+        //Turns WiFI on when the app starts if it is off
+        if (!wifiManager.isWifiEnabled()) {
+            wifiManager.setWifiEnabled(true);
+        }
+    }
 
-
+    private void autoListener() {
+        turnWifiOn();
+        discoverPeers();
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -146,6 +155,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
         btnClear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick (View v) {
@@ -160,29 +170,15 @@ public class MainActivity extends AppCompatActivity {
                 if (wifiManager.isWifiEnabled()) {
                     wifiManager.setWifiEnabled(false);
                     btnOnOff.setText("TURN WIFI ON");
+                    isConnected = false;
                 } else {
                     wifiManager.setWifiEnabled(true);
                     btnOnOff.setText("TURN WIFI OFF");
+                    discoverPeers();
+
                 }
             }
         });
-
-    }
-
-    private void exqListener() {
-        btnOnOff.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (wifiManager.isWifiEnabled()) {
-                    wifiManager.setWifiEnabled(false);
-                    btnOnOff.setText("TURN WIFI ON");
-                } else {
-                    wifiManager.setWifiEnabled(true);
-                    btnOnOff.setText("TURN WIFI OFF");
-                }
-            }
-        });
-
         btnDiscover.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -199,6 +195,28 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
         });
+
+    }
+
+
+
+    private void exqListener() {
+        btnOnOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (wifiManager.isWifiEnabled()) {
+                    wifiManager.setWifiEnabled(false);
+                    btnOnOff.setText("TURN WIFI ON");
+                } else {
+                    wifiManager.setWifiEnabled(true);
+                    btnOnOff.setText("TURN WIFI OFF");
+                }
+            }
+        });
+
+
+
+
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -242,30 +260,33 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void connectToOtherTablet(final WifiP2pDevice device, int position ) { //device device
 
-        //final WifiP2pDevice device = deviceArray[position];
-        //device = deviceArray[position];
-        WifiP2pConfig config = new WifiP2pConfig();
-        config.deviceAddress = device.deviceAddress;
+    public void connect() {
+        // Picking the first device found on the network.
+        if (peers.size()>0) {
+            final WifiP2pDevice device = peers.get(0);
 
-        mManager.connect(mChannel, config, new WifiP2pManager.ActionListener() {
-            @Override
-            public void onSuccess() {
-                Toast.makeText(getApplicationContext(), "Connected To " + device.deviceName,Toast.LENGTH_SHORT).show();
-                connectionSuccessful();
+            WifiP2pConfig config = new WifiP2pConfig();
+            config.deviceAddress = device.deviceAddress;
+            config.wps.setup = WpsInfo.PBC;
 
+            mManager.connect(mChannel, config, new WifiP2pManager.ActionListener() {
 
-            }
+                @Override
+                public void onSuccess() {
+                    // WiFiDirectBroadcastReceiver notifies us. Ignore for now.
+                    Toast.makeText(getApplicationContext(), "Connected To" + device.deviceName, Toast.LENGTH_SHORT).show();
+                }
 
-            @Override
-            public void onFailure(int reason) {
-                Toast.makeText(getApplicationContext(), "Not Connected",Toast.LENGTH_SHORT).show();
-
-            }
-        });
-
+                @Override
+                public void onFailure(int reason) {
+                    Toast.makeText(getApplicationContext(), "Connect failed. Retry.", Toast.LENGTH_SHORT).show();
+                    isConnected = false;
+                }
+            });
+        }
     }
+
 
     private void connectionSuccessful() {
         connection2.getBackground().setColorFilter(Color.parseColor("#41A317"), PorterDuff.Mode.SRC_IN);
@@ -273,6 +294,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void connectionUnsuccessful() {
         connection2.getBackground().setColorFilter(Color.parseColor("#818181"), PorterDuff.Mode.SRC_IN);
+        isConnected = false;
     }
 
 
@@ -323,49 +345,23 @@ public class MainActivity extends AppCompatActivity {
 
                 ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1,deviceNameArray);
                 listView.setAdapter(adapter);
+
             }
 
-            //connectToOtherTablet(device, index);
+            if (isConnected == false && peers.size()>0) {
+                connect();
+                isConnected = true;
+            }
+
 
             if (peers.size() ==0) {
                 Toast.makeText(getApplicationContext(),"No Device Found", Toast.LENGTH_SHORT).show();
                 connectionUnsuccessful();
                 return;
             }
+
         }
-    };
 
-
-    //WifiP2pManager.PeerListListener peerListListener2 = new WifiP2pManager.PeerListListener() {
-    WifiP2pManager.PeerListListener peerListListener2 = new WifiP2pManager.PeerListListener() {
-        @Override
-        public void onPeersAvailable(WifiP2pDeviceList peerList) {
-            //Checks if current peers matches what is displaying in ListView
-            if(!peerList.getDeviceList().equals(peers)){
-                peers.clear();
-                peers.addAll(peerList.getDeviceList());
-
-
-                deviceNameArray = new String[peerList.getDeviceList().size()];
-                deviceArray = new WifiP2pDevice[peerList.getDeviceList().size()];
-                int index = 0;
-
-                for (WifiP2pDevice device : peerList.getDeviceList()){
-                    deviceNameArray[index] = device.deviceName;
-                    deviceArray[index] = device;
-                    index++;
-                }
-
-                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1,deviceNameArray);
-                listView.setAdapter(adapter);
-            }
-
-            if (peers.size() ==0) {
-                Toast.makeText(getApplicationContext(),"No Device Found", Toast.LENGTH_SHORT).show();
-                connectionUnsuccessful();
-                return;
-            }
-        }
     };
 
 
@@ -402,14 +398,21 @@ public class MainActivity extends AppCompatActivity {
         unregisterReceiver(mReceiver);
     }
 
+
     public class ServerClass extends Thread{
-        Socket socket;
+        Socket socket = null;
         ServerSocket serverSocket;
+
 
         @Override
         public void run() {
+
             try {
-                serverSocket = new ServerSocket(8888);
+                serverSocket = new ServerSocket(); // <-- create an unbound socket first
+                serverSocket.setReuseAddress(true);
+                serverSocket.bind(new InetSocketAddress(PORT_NUMBER)); // <-- now bind it
+
+                //serverSocket = new ServerSocket(8888);
                 socket = serverSocket.accept();
                 sendReceive = new SendReceive(socket);
                 sendReceive.start();
@@ -464,7 +467,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     public class ClientClass extends Thread {
-        Socket socket;
+        Socket socket = null;
         String hostAdd;
 
         public ClientClass (InetAddress hostAddress)
@@ -476,7 +479,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void run() {
             try {
-                socket.connect(new InetSocketAddress(hostAdd,8888),500);
+                socket.connect(new InetSocketAddress(hostAdd,PORT_NUMBER),500);
                 sendReceive = new SendReceive(socket);
                 sendReceive.start();
             } catch (IOException e) {
